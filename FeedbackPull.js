a2efe83193ee42a1477ed4d2e731a499cc3b2c16
RaulMedeiros <rmedeirosperez@gmail.com>
2022-11-04 10:18:25 +0100

    Google analytics api calls fixed

--- FeedbackPull.js
+++ FeedbackPull.js
@@ -8,63 +8,92 @@
 // context object of pure::variants
 var root = pure_variants().getContextModel().getRoot();
 
-
 var feature_children = root.getChildren();
 var feedback_attributes = {};
-process_children(feature_children,feedback_attributes);
+process_children(feature_children, feedback_attributes);
 //feedback_results =  get_results_from_GA(feedback_results);
-
-
+//print_dict(feedback_attributes)
 
 var isWindows = java.lang.System.getProperty("os.name")
     .toLowerCase().startsWith("windows");
-
 var processBuilder = new java.lang.ProcessBuilder();
-processBuilder.command("bash", "-c", "touch /Users/RaulMedeiros/Desktop/testeo_scopious.txt");
+processBuilder.environment().put("PATH", "/usr/local/opt/swagger-codegen@2/bin:/usr/local/opt/swagger-codegen@2/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/TeX/texbin");
+for (var key in feedback_attributes) {
+    try {
 
+        //processBuilder.command("bash", "-c", "pwd");
+        processBuilder.command("bash", "-c", "node /Users/RaulMedeiros/git/SCOPIOUS/ga_api_requests/index.js '" + feedback_attributes[key] + "'");
+        var process = processBuilder.start();
+        var output = new java.lang.StringBuilder();
 
+        var reader = new java.io.BufferedReader(
+            new java.io.InputStreamReader(process.getInputStream()));
 
+        var line;
+        while ((line = reader.readLine()) != null) {
+            output.append(line + "\n");
+        }
 
-var process = processBuilder.start();
-var output = new java.lang.StringBuilder();
-
-var reader = new java.io.BufferedReader(
-    new java.io.InputStreamReader(process.getInputStream()));
-
-var line;
-while ((line = reader.readLine()) != null) {
-    output.append(line + "\n");
+        var exitVal = process.waitFor();
+        if (exitVal == 0) {
+            feedback_attributes[key] = output;
+        } else {
+            console().println("Fail!");
+            console().println(output);
+        }
+    } catch (e) {
+        console().println(e);
+    }
 }
+var pathToModel = pure_variants().getPathOfContextProject() + "/scopious-feedback.xfm";
+//pure_variants().deleteModel(pathToModel);
+pure_variants().createModel(pathToModel, "scopious", ModelConstants().FM_TYPE);
+var scopiousModel = pure_variants().openModel(pathToModel);
 
-var exitVal = process.waitFor();
-if (exitVal == 0) {
-    console().println("Success!");
-    console().println(output);
-} else {
-    //abnormal...
+try {
+    var operation = pure_variants().changeModel(scopiousModel);
+    operation.addElement(root, scopiousModel.getRoot(), "ps:feature");
+    addChildren(scopiousModel.getRoot().getChildren(), root.getChildren(), operation )
+    pure_variants().updateModel(operation);
+    pure_variants().saveModel(scopiousModel);
+} catch (e) {
+    console().println(e);
+} finally {
+    pure_variants().closeModel(scopiousModel);
 }
+console().println("finished!");
 
 
-
-
-function process_children(elements,feedback_attributes) {
+function process_children(elements, feedback_attributes) {
     var idx = elements.iterator();
     while (idx.hasNext()) {
         var child_element = idx.next();
-        process_feedback_attributes(child_element.getPropertyList().iterator(),feedback_attributes)
-        process_children(child_element.getChildren(),feedback_attributes);
+        process_feedback_attributes(child_element.getPropertyList().iterator(), feedback_attributes)
+        process_children(child_element.getChildren(), feedback_attributes);
     }
 }
 
 
-function process_feedback_attributes(attributes,feedback_attributes) {
+function process_feedback_attributes(attributes, feedback_attributes) {
     while (attributes.hasNext()) {
         var attribute = attributes.next();
         if (attribute.getName().startsWith("scop_")) {
             var value = attribute.getValueList().iterator().next().getContent();
-            feedback_attributes[attribute.getName()]=value;
+            feedback_attributes[attribute.getName()] = value;
         }
 
     }
 }
 
+
+function print_dict(dict) {
+    console().println("hello");
+    for (var key in dict) {
+        console().println(dict[key] + ":" + key)
+    }
+}
+
+
+function addChildren(originalChildren, scopiousChildren, operation) {
+
+}
--- ga_api_requests/index.js
+++ ga_api_requests/index.js
@@ -1,23 +1,42 @@
 const {google} = require('googleapis')
 
-require('dotenv').config({path : process.argv.slice(2)[0]})
+require('dotenv').config({path : "/Users/RaulMedeiros/git/SCOPIOUS/ga_api_requests/.env"})
 
 const scopes = 'https://www.googleapis.com/auth/analytics.readonly'
 
-const auth = new google.auth.GoogleAuth({keyFile: './gaff.json', scopes: scopes})
+const auth = new google.auth.GoogleAuth({keyFile: '/Users/RaulMedeiros/git/SCOPIOUS/ga_api_requests/gaff.json', scopes: scopes})
 
 const viewId = process.env.VIEW_ID
-
-async function getData() {
+let query = new Object()
+const scopiousQuery =  process.argv[2]
+async function getData(query) {
     const result = await google.analytics('v3').data.ga.get({
         'auth': auth,
         'ids': 'ga:' + viewId,
-        'start-date': '7daysAgo',
+        'start-date': '2022-02-01',
         'end-date': 'today',
-        'dimensions': 'ga:eventCategory,ga:dimension1',
-        'metrics': 'ga:totalEvents',
-        'filters': 'ga:eventCategory==Commenting',
+        'dimensions': query.dimensions,
+        'metrics': query.metrics,
+        'filters': query.filters,
     })
     console.dir(result.data.rows)
 }
-getData()
+
+
+function getDimensions(scopiousQuery) {
+    let dimensions = scopiousQuery.split("dimensions=")[1].split(";")[0]
+    return dimensions;
+}
+function getMetrics(scopiousQuery) {
+    let metrics = scopiousQuery.split("metrics=")[1].split(";")[0]
+    return metrics;
+}
+
+function getFilters(scopiousQuery) {
+    let filters = scopiousQuery.split("filters=")[1]
+    return filters;
+}
+query.dimensions = getDimensions(scopiousQuery)
+query.metrics = getMetrics(scopiousQuery)
+query.filters = getFilters(scopiousQuery)
+getData(query)
